6806962edceb3442a2464029b5d0369fe20f2d23
Lau <lau.priego@gmail.com>
2015-05-02 20:03:00 +0200

    Clase Asignatura implementada

--- unit8-master/src/Asignatura.java
+++ unit8-master/src/Asignatura.java
@@ -1,5 +1,40 @@
-
+/**
+ * 
+ * @author LAURA MARTINEZ PRIEGO 1oDAM
+ *
+ */
 public class Asignatura
 {
 
+	private String nombreAsignatura;
+	private double nota;
+	
+	
+	public Asignatura(String nombreAsignatura)
+	{
+		this.nombreAsignatura = nombreAsignatura;
+	}
+	
+	//GETTER
+	public String getNombre()
+	{
+		return nombreAsignatura;
+	}
+	
+	public double getNota()
+	{
+		return nota;
+	}
+	
+	//SETTERS
+	public void setNombre( String nombreAsignaturaString)
+	{
+		this.nombreAsignatura = nombreAsignaturaString;
+	}
+	
+	public void setNota( double nota)
+	{
+		this.nota = nota;
+	}
 }
+
